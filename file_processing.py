757a9154023345c705fc73d07a3ab4c2a0562dce
Sergi Sorribas <sergisj@tid.es>
2012-09-19 18:12:54 +0200

    [ADDED] test with print

--- file_processing.py
+++ file_processing.py
@@ -6,24 +6,23 @@ Copyright (c) Telefonica I+D. All rights reserved.
 Description: Example of processing files with encoding
 '''
 
-import sqlite3;
-
 def insert_line(cursor, line):
     sql = 'INSERT INTO CUSTOMERS VALUES (?,?,?,?,?,?,?,?,?)'
     cursor.execute(sql, line)
 
 
-def process_file_db(file_path):
-    with sqlite3.connect('test.db') as connection:
+def process_file_db(connection, file_path):
+    try:
         cursor = connection.cursor()
         with open(file_path, 'r') as opened_file:
             headers = opened_file.readline()
             for line in opened_file.readlines():
                 insert_line(cursor, line.rstrip().split('|'))
+    finally:
         cursor.close()
     
-def process_csv_db(file_path):
-    with sqlite3.connect('test.db') as connection:
+def process_csv_db(connection, file_path):
+    try:
         connection.text_factory = str
         cursor = connection.cursor()
         with open(file_path, 'r') as opened_file:
@@ -32,16 +31,19 @@ def process_csv_db(file_path):
             headers = reader.next()
             for line in reader:
                 insert_line(cursor, line)
+    finally:
         cursor.close()
     
-def process_codecs_db(file_path):
-    with sqlite3.connect('test.db') as connection:
+def process_codecs_db(connection, file_path):
+    try:
         cursor = connection.cursor()
         import codecs
         with codecs.open(file_path) as opened_file:
             for i, line in enumerate(opened_file.readlines()):
                 if i != 0:
                     insert_line(cursor, line.rstrip().split('|'))
+    finally:
+        cursor.close()
 
 def process_codecs_file(file_path_in, file_path_out):
     import codecs
--- test_file_processing.py
+++ test_file_processing.py
@@ -3,78 +3,89 @@
 
 import file_processing as exercise
 import unittest
+
 import sqlite3
 
 class TestExercise(unittest.TestCase):
     """Test for exercise"""
 
     def setUp(self):
-        with sqlite3.connect('test.db') as connection:
-            cursor = connection.cursor()
-            cursor.execute('DELETE FROM customers')
-            cursor.close()
+        self.connection = sqlite3.connect('test.db')
+        cursor = self.connection.cursor()
+        cursor.execute('DELETE FROM customers')
+        cursor.close()
+    
+    def tearDown(self):
+        self.connection.close()
+
 
     def test_process_file_db_utf8(self):
         """ Try to process a file with open builtin function and inserting into
         db"""
         file_path = 'test-utf8.dat'
-        exercise.process_file_db(file_path)
-        with sqlite3.connect('test.db') as connection:
-            connection.text_factory = str
-            cursor = connection.cursor()
-            cursor.execute('SELECT * FROM CUSTOMERS WHERE NAME LIKE "Magda%"')
-            self.assertEqual(cursor.fetchone()[0].decode('utf-8'), u'Magdalena Díaz')
+        self.connection.text_factory = str
+        exercise.process_file_db(self.connection, file_path)
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM CUSTOMERS WHERE NAME LIKE "Magda%"')
+        self.assertEqual(cursor.fetchone()[0].decode('utf-8'), u'Magdalena Díaz')
+        cursor.close()
 
     def test_process_file_db_latin1(self):
         """ Try to process a file with open builtin function and inserting into
         db. Test with latin1 input file."""
         file_path = 'test-latin1.dat'
-        exercise.process_file_db(file_path)
-        with sqlite3.connect('test.db') as connection:
-            connection.text_factory = str
-            cursor = connection.cursor()
-            cursor.execute('SELECT * FROM CUSTOMERS WHERE NAME LIKE "Magda%"')
-            self.assertEqual(cursor.fetchone()[0].decode('utf-8'), u'Magdalena Díaz')
+        self.connection.text_factory = str
+        exercise.process_file_db(self.connection, file_path)
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM CUSTOMERS WHERE NAME LIKE "Magda%"')
+        self.assertEqual(cursor.fetchone()[0].decode('utf-8'), u'Magdalena Díaz')
+        cursor.close()
             
+    def test_process_file_utf8(self):
+        with open('test-utf8.dat', 'r') as filep:
+            for line in filep.readlines():
+                print line
+                print 'Decoded line: {0}'.format(line.decode('utf-8'))
+
     def test_process_csv_db_utf8(self):
         """ Try to process a file with csv module and inserting into
         db"""
         file_path = 'test-utf8.dat'
-        exercise.process_csv_db(file_path)
-        with sqlite3.connect('test.db') as connection:
-            cursor = connection.cursor()
-            cursor.execute('SELECT * FROM CUSTOMERS WHERE NAME LIKE "Magda%"')
-            self.assertEqual(cursor.fetchone()[0], u'Magdalena Díaz')
+        exercise.process_csv_db(self.connection, file_path)
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM CUSTOMERS WHERE NAME LIKE "Magda%"')
+        self.assertEqual(cursor.fetchone()[0], u'Magdalena Díaz')
+        cursor.close()
 
     def test_process_csv_db_latin1(self):
         """ Try to process a file with csv module and inserting into
         db. Test with latin1 input file."""
         file_path = 'test-latin1.dat'
-        exercise.process_csv_db(file_path)
-        with sqlite3.connect('test.db') as connection:
-            cursor = connection.cursor()
-            cursor.execute('SELECT * FROM CUSTOMERS WHERE NAME LIKE "Magda%"')
-            self.assertEqual(cursor.fetchone()[0], u'Magdalena Díaz')
+        exercise.process_csv_db(self.connection, file_path)
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM CUSTOMERS WHERE NAME LIKE "Magda%"')
+        self.assertEqual(cursor.fetchone()[0], u'Magdalena Díaz')
+        cursor.close()
 
     def test_process_codecs_db_utf8(self):
         """ Try to process a file with codecs module and inserting into
         db."""
         file_path = 'test-utf8.dat'
-        exercise.process_codecs_db(file_path)
-        with sqlite3.connect('test.db') as connection:
-            cursor = connection.cursor()
-            cursor.execute('SELECT * FROM CUSTOMERS WHERE NAME LIKE "Magda%"')
-            self.assertEqual(cursor.fetchone()[0], u'Magdalena Díaz')
+        exercise.process_codecs_db(self.connection, file_path)
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM CUSTOMERS WHERE NAME LIKE "Magda%"')
+        self.assertEqual(cursor.fetchone()[0], u'Magdalena Díaz')
+        cursor.close()
 
     def test_process_codecs_db_latin1(self):
         """ Try to process a file with codecs module and inserting into
         db. Test with latin1 file."""
         file_path = 'test-latin1.dat'
-        exercise.process_codecs_db(file_path)
-        with sqlite3.connect('test.db') as connection:
-            cursor = connection.cursor()
-            cursor.execute('SELECT * FROM CUSTOMERS WHERE NAME LIKE "Magda%"')
-            self.assertEqual(cursor.fetchone()[0], u'Magdalena Díaz')
+        exercise.process_codecs_db(self.connection, file_path)
+        cursor = self.connection.cursor()
+        cursor.execute('SELECT * FROM CUSTOMERS WHERE NAME LIKE "Magda%"')
+        self.assertEqual(cursor.fetchone()[0], u'Magdalena Díaz')
+        cursor.close()
 
     def test_process_codecs_file_utf8(self):
         """ Process a file and write it to another file. output in utf-8
@@ -93,12 +104,13 @@ class TestExercise(unittest.TestCase):
         """
         in_file = 'test-latin1.dat'
         out_file = 'test-out-utf8.dat'
-        exercise.process_codecs_file(in_file, out_file, 'latin-1', 'utf8')
+        exercise.process_codecs_file(in_file, out_file)
         with open(out_file) as assert_file:
             for line in assert_file:
                 name = line.rstrip().split('|')[0]
                 if name.startswith('Mag'):
                     self.assertEqual(name, u'Magdalena Díaz')
+
 
 
 if __name__ == '__main__':
